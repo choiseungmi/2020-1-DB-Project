e942e140d8dbe4c2f9dae31efe935150d92266ea
seungmi <2018102242@khu.ac.kr>
2020-06-06 20:43:12 +0900

    Start project

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const request = require('request');
+const app = express();
+var fs = require("fs");
+app.use(express.static('public'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.text());
+app.use(bodyParser.urlencoded({
+  extended: false
+})); //post에서bodyparser로 받기 위함
+
+var price;
+var shape = [];
+var kinds = [];
+
+app.set('port', (process.env.PORT || 5000));
+
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  res.render('index.ejs', {title: "슬기로운 격리생활", description:"dsgd",randvalue:'sds' });
+})
+
+
+app.listen(app.get('port'), () => {
+  console.log('running on port', app.get('port'));
+})
