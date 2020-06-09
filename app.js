11c369e384c6068a9663cb3ddc9ddc77911eb15e
seungmi <2018102242@khu.ac.kr>
2020-06-09 19:17:08 +0900

    login API

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ const app = express();
 const server = http.createServer(app);
 const fs = require("fs");
 const oracledb = require("oracledb");
-// app.use(express.static('views/statepage'));
+var cookieParser = require('cookie-parser');
+var expressSession = require('express-session');
 app.use(express.static('views'));
 // app.set('views', './views/statepage');
 app.engine('html', require('ejs').renderFile);
@@ -18,7 +19,9 @@ app.use(bodyParser.text());
 app.use(bodyParser.urlencoded({
   extended: false
 })); //post에서bodyparser로 받기 위함
-// var router = express.Router();
+
+var router = express.Router();
+
 // 오라클 접속
 oracledb.getConnection({
   user: process.env.DB_USER,
@@ -31,11 +34,34 @@ oracledb.getConnection({
   conn = con;
 });
 
+//쿠키와 세션을 미들웨어로 등록한다
+app.use(cookieParser());
+
+//세션 환경 세팅
+app.use(expressSession({
+  secret: 'my key', //이때의 옵션은 세션에 세이브 정보를 저장할때 할때 파일을 만들꺼냐
+  //아니면 미리 만들어 놓을꺼냐 등에 대한 옵션들임
+  resave: true,
+  saveUninitialized: true
+}));
+
+app.use(function(req, res, next) {
+     res.locals.user = req.session.user;
+     next();
+});
 
 app.get('/', (req, res) => {
-  res.render('index.ejs',{message:''});
+  res.render('index.ejs', {
+    message: ''
+  });
+})
+app.get('/login', (req, res) => {
+  res.render('statepage/login.html');
 })
 
+app.get("/signup", function(req, res) {
+  res.render('register.ejs');
+})
 app.post("/singup", function(request, response) {
   console.log(request.body);
   //오라클에 접속해서 insert문을 실행한다.
@@ -67,38 +93,116 @@ app.post("/singup", function(request, response) {
 app.post('/login', (req, res) => {
   var id = req.body.ID
   var password = req.body.password;
-  // var sql = "SELECT password FROM accounts where id=tester";
-  conn.execute(`select password from accounts where user_id='${id}'`, function(err, result) {
-    if (err) {
-      console.log("로그인 중 에러가 발생했어요!!", err);
-      console.log("fail: " + id + password);
-      res.writeHead(400, {
-        "ContentType": "text/html"
-      });
-      res.render('statepage/404.html');
-    } else {
-      console.log(result, result.rows.length);
-      console.log("output:", result.rows[0], trim(password));
-      if (result.rows.length > 0) {
-        if (password == trim(result.rows[0][0])) {
-          res.render('detail.ejs');
-          console.log("sucess: " + id + password);
-        } else {
-          console.log("wrong: " + id + password);
-          res.render('index.ejs',{message:'비밀번호가 틀렸습니다'});
-        }
+
+  if (req.session.user) {
+    console.log('이미 로그인 되어 있음');
+    res.render('detail.ejs');
+  } else {
+    conn.execute(`select password, name from accounts where user_id='${id}'`, function(err, result) {
+      if (err) {
+        console.log("로그인 중 에러가 발생했어요!!", err);
+        console.log("fail: " + id + password);
+        res.writeHead(400, {
+          "ContentType": "text/html"
+        });
+        res.render('statepage/404.html');
       } else {
-        res.render('index.ejs',{message:'존재하지 않는 아이디입니다'});
-        res.end("fail!!");
-        console.log("0<user: " + id + password);
+        console.log(result, result.rows.length);
+        console.log("output:", result.rows, trim(password));
+        if (result.rows.length > 0) {
+          if (password == trim(result.rows[0][0])) {
+            req.session.user =
+                {
+                    id: id,
+                    pw: password,
+                    name: result.rows[0][1],
+                    authorized: true
+                };
+            res.render('detail.ejs');
+            console.log("sucess: " + id + password);
+            console.log(req.session.user['name']);
+          } else {
+            console.log("wrong: " + id + password);
+            res.render('index.ejs', {
+              message: '비밀번호가 틀렸습니다'
+            });
+          }
+        } else {
+          res.render('index.ejs', {
+            message: '존재하지 않는 아이디입니다'
+          });
+          res.end("fail!!");
+          console.log("0<user: " + id + password);
+        }
+        // console.log(result.metaData);  //테이블 스키마
+        //         console.log(result.rows);
       }
-      // console.log(result.metaData);  //테이블 스키마
-      //         console.log(result.rows);
-    }
-  });
-
+    });
+  }
 })
+router.route('/detail').get(
+  function(req, res) {
+    console.log('/process/product  라우팅 함수 실행');
 
+    //세션정보는 req.session 에 들어 있다
+    if (req.session.user) //세션에 유저가 있다면
+    {
+      res.render('detail.ejs');
+    } else {
+      res.redirect('/');
+
+    }
+  }
+);
+
+
+router.route('/logout').get( //설정된 쿠키정보를 본다
+  function(req, res) {
+    console.log('/process/loginout 라우팅 함수호출 됨');
+
+    if (req.session.user) {
+      console.log('로그아웃 처리');
+      req.session.destroy(
+        function(err) {
+          if (err) {
+            console.log('세션 삭제시 에러');
+            return;
+          }
+          console.log('세션 삭제 성공');
+          //파일 지정시 제일 앞에 / 를 붙여야 root 즉 public 안에서부터 찾게 된다
+          res.redirect('/');
+        }
+      ); //세션정보 삭제
+
+    } else {
+      console.log('로긴 안되어 있음');
+      res.redirect('/');
+    }
+
+
+
+  }
+);
+
+
+//라우터 미들웨어 등록하는 구간에서는 라우터를 모두  등록한 이후에 다른 것을 세팅한다
+//그렇지 않으면 순서상 라우터 이외에 다른것이 먼저 실행될 수 있다
+app.use('/', router); //라우트 미들웨어를 등록한다
+
+
+app.all('*',
+  function(req, res) {
+    res.status(404).send('<h1> 요청 페이지 없음 </h1>');
+  }
+);
+//
+// //웹서버를 app 기반으로 생성
+// var appServer = http.createServer(app);
+// appServer.listen(app.get('port'),
+//     function () {
+//         console.log('express 웹서버 실행' + app.get('port'));
+//     }
+// );
 function trim(value) {
   value = value.replace(/\s+/, ""); //왼쪽 공백제거
   value = value.replace(/\s+$/g, ""); //오른쪽 공백제거
@@ -108,6 +212,6 @@ function trim(value) {
 }
 app.set('port', (process.env.HOST || 5000));
 
-  app.listen(app.get('port'), () => {
-    console.log('running on port', app.get('port'));
-  })
+app.listen(app.get('port'), () => {
+  console.log('running on port', app.get('port'));
+})
--- routes/user.js
+++ routes/user.js
@@ -1,92 +1,148 @@
-const express = require('express');
-const router = express.Router();
-const oracle = require('../oracle.js');
-//
-// const sql = "select *  from accounts";
-//
-//
-// router.get('/sign_up', function(req, res, next) {
-//   res.render("register.html");
-// });
-//
-//
-// router.post("/sign_up", function(req, res, next) {
-//   let body = req.body;
-//
-//   const sql = "insert accounts (user_id, name, email, password, address) value (" || body.id || ", " || body.userName || ", " || body.userEmail || ", " || body.password || ", null)"
-//   oracle.queryObject(sql, {}, {})
-//     .then(function(result) {
-//       console.log(result.rows[0]);
-//     })
-//     .catch(function(err) {
-//       next(err);
-//     })
-//     .then(result => {
-//       res.redirect("/");
-//     })
-//     .catch(err => {
-//       console.log(err)
-//     })
-// })
-//
-// module.exports = router;
+var express = require('express');
+var http = require('http');
+var serveStatic = require('serve-static');      //특정 폴더의 파일들을 특정 패스로 접근할 수 있도록 열어주는 역할
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var expressSession = require('express-session');
+
+var app = express();      //express 서버 객체
+
+var bodyParser_post = require('body-parser');       //post 방식 파서
+
+app.set('port', 3000);
 
 
-exports.register = function (req, res) {
-    // console.log("req", req.body);
-    var today = new Date();
-    var users = {
-        "user_id": req.body.id,
-        "name": req.body.last_name,
-        "email": req.body.email,
-        "password": req.body.password,
-        "address": today
+//미들웨어들 등록 시작, 아래 미들웨어들은 내부적으로 next() 가실행됨
+
+//join은 __dirname : 현재 .js 파일의 path 와 public 을 합친다
+//이렇게 경로를 세팅하면 public 폴더 안에 있는것을 곧바로 쓸 수 있게된다
+app.use(serveStatic(path.join(__dirname, 'public')));
+
+
+//post 방식 일경우 begin
+//post 의 방식은 url 에 추가하는 방식이 아니고 body 라는 곳에 추가하여 전송하는 방식
+app.use(bodyParser_post.urlencoded({ extended: false }));            // post 방식 세팅
+app.use(bodyParser_post.json());                                     // json 사용 하는 경우의 세팅
+//post 방식 일경우 end
+
+
+
+//쿠키와 세션을 미들웨어로 등록한다
+app.use(cookieParser());
+
+//세션 환경 세팅
+//세션은 서버쪽에 저장하는 것을 말하는데, 파일로 저장 할 수도 있고 레디스라고 하는 메모리DB등 다양한 저장소에 저장 할 수가 있는데
+app.use(expressSession({
+    secret: 'my key',           //이때의 옵션은 세션에 세이브 정보를 저장할때 할때 파일을 만들꺼냐
+                                //아니면 미리 만들어 놓을꺼냐 등에 대한 옵션들임
+    resave: true,
+    saveUninitialized:true
+}));
+
+
+
+//라우트를 미들웨어에 등록하기 전에 라우터에 설정할 경로와 함수를 등록한다
+//
+//라우터를 사용 (특정 경로로 들어오는 요청에 대하여 함수를 수행 시킬 수가 있는 기능을 express 가 제공해 주는것)
+var router = express.Router();
+
+
+//http://localhost:3000/process/product 이 주소로 치면 라우터를 통해 바로 여기로 올 수 있다
+router.route('/process/product').get(
+    function (req, res)
+    {
+        console.log('/process/product  라우팅 함수 실행');
+
+        //세션정보는 req.session 에 들어 있다
+        if (req.session.user)       //세션에 유저가 있다면
+        {
+            res.redirect('/product.html');
+        }
+        else
+        {
+            res.redirect('/login2.html');
+
+        }
     }
-    const sql = "insert accounts (user_id, name, email, password, address) value ("||users|| ", null)";
-     oracle.queryObject(sql, {}, {})
-       .then(function(result) {
-         console.log(result.rows[0]);
-       })
-       .catch(function(err) {
-         next(err);
-       })
-       .then(result => {
-         res.redirect("/");
-       })
-       .catch(err => {
-         console.log(err)
-       });
-}
+);
 
-exports.login = function (req, res) {
-  res.redirect("/login");
-    var id = req.body.ID;
-    var password = req.body.password;
-    const sql = "SELECT password FROM accounts where id="||id;
-    oracle.queryObject(sql, {}, {})
-      .then(function(result) {
-        if(result==password){
-          res.send({
-              "code": 200,
-              "success": "login sucessfull"
-          });
-          console.log("success");
+router.route('/process/login').post(                      //설정된 쿠키정보를 본다
+    function (req, res) {
+        console.log('/process/login 라우팅 함수호출 됨');
+
+        var paramID = req.body.id || req.query.id;
+        var pw = req.body.passwords || req.query.passwords;
+
+        if (req.session.user) {
+            console.log('이미 로그인 되어 있음');
+
+            res.writeHead(200, { "Content-Type": "text/html;characterset=utf8" });
+            res.write('<h1>already Login</h1>');
+            res.write('[ID] : ' + paramID + ' [PW] : ' + pw);
+            res.write('<a href="/process/product">Move</a>');
+            res.end();
+
+        } else {
+            req.session.user =
+                {
+                    id: paramID,
+                    pw: pw,
+                    name: 'UsersNames!!!!!',
+                    authorized: true
+                };
+            res.writeHead(200, { "Content-Type": "text/html;characterset=utf8" });
+            res.write('<h1>Login Success</h1>');
+            res.write('[ID] : ' + paramID + ' [PW] : ' + pw);
+            res.write('<a href="/process/product">Move</a>');
+            res.end();
         }
-        else{
-          res.send({
-              "code": 400,
-              "failed": "error ocurred"
-          })
-          console.log("fail");
+    }
+);
+
+router.route('/process/logout').get(                      //설정된 쿠키정보를 본다
+    function (req, res) {
+        console.log('/process/loginout 라우팅 함수호출 됨');
+
+        if (req.session.user) {
+            console.log('로그아웃 처리');
+            req.session.destroy(
+                function (err) {
+                    if (err) {
+                        console.log('세션 삭제시 에러');
+                        return;
+                    }
+                    console.log('세션 삭제 성공');
+                    //파일 지정시 제일 앞에 / 를 붙여야 root 즉 public 안에서부터 찾게 된다
+                    res.redirect('/Login2.html');
+                }
+            );          //세션정보 삭제
+
+        } else {
+            console.log('로긴 안되어 있음');
+            res.redirect('/Login2.html');
         }
-      })
-      .catch(function(err) {
-        next(err);
-      })
-      .then(result => {
-        res.redirect("/");
-      })
-      .catch(err => {
-        console.log(err)
-      });
-}
+
+
+
+    }
+);
+
+
+//라우터 미들웨어 등록하는 구간에서는 라우터를 모두  등록한 이후에 다른 것을 세팅한다
+//그렇지 않으면 순서상 라우터 이외에 다른것이 먼저 실행될 수 있다
+app.use('/', router);       //라우트 미들웨어를 등록한다
+
+
+app.all('*',
+    function (req, res) {
+        res.status(404).send('<h1> 요청 페이지 없음 </h1>');
+    }
+);
+
+//웹서버를 app 기반으로 생성
+var appServer = http.createServer(app);
+appServer.listen(app.get('port'),
+    function () {
+        console.log('express 웹서버 실행' + app.get('port'));
+    }
+);
